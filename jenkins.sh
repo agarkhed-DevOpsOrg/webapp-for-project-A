fc821ceddb0f9d1f3f2f785a57818091101d031c
ashwin <ashwin.p.agarkhed@gmail.com>
2020-09-17 23:40:09 -0400

    making two anible-playbooks

--- jenkins.sh
+++ jenkins.sh
@@ -1,2 +1,11 @@
 # this is the script which I will run as pos deplyment actions for jenkins
-ansible-playbook -i /etc/docker/hosts ansible-playbook.yml
+ansible-playbook -i /etc/docker/hosts image-playbook.yml --limit localhost
+ansible-playbook -i /etc/docker/hosts ansible-playbook.yml --limit target
+
+
+#$ cat hosts
+#localhost
+#
+#[target]
+#3.83.253.142
+#3.88.50.68
