d54e37698c94e003c594290a344aa0434a3c174d
ashwin <ashwin.p.agarkhed@gmail.com>
2020-09-27 12:51:14 -0400

    giving correct path for hosts in playbook

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 # this is the script which I will run as pos deplyment actions for jenkins
 
 #
-cd /opt/docker
+
 ansible-playbook -i hosts image-playbook.yml
-ansible-playbook -i /etc/docker/hosts ansible-playbook.yml
+ansible-playbook -i /etc/ansible/hosts ansible-playbook.yml
