287c59c24949d4ac6d07ebd9d00a1eac91c8a12f
ashwin <ashwin.p.agarkhed@gmail.com>
2020-09-27 13:26:24 -0400

    changes

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 # this is the script which I will run as pos deplyment actions for jenkins
 
-#
+#makking rnadom canges
 
 ansible-playbook -i hosts image-playbook.yml
 ansible-playbook -i /etc/ansible/hosts ansible-playbook.yml
